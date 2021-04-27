bf53d2d508eee75b14bc5a5a21988dddd6559dce
ragulhchc <ragulhchc@gmail.com>
2021-04-27 16:35:22 +0530

    commit

--- Ideathon/App.js
+++ Ideathon/App.js
@@ -5,7 +5,10 @@ import Demo from './Screens/Demo'
 
 export default function App() {
   return (
-    <Demo/>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
