7afbc60978f569d5fc0d2d8a412809e5f1cefeaa
ragulhchc <ragulhchc@gmail.com>
2021-04-27 22:02:41 +0530

    added screen file

--- Ideathon/App.js
+++ Ideathon/App.js
@@ -5,10 +5,7 @@ import Demo from './Screens/Demo'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Demo/>
   );
 }
 
---
+++ Ideathon/Screens/Demo.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Button, Image } from 'react-native';
+
+export default function Demo() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="click"></Button>
+      <Image
+        style={styles.logo}
+        source={{
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
