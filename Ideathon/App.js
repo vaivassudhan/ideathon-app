eea7d044ea8b789bdf6337adf77a8b630ef369bf
ragulhchc <ragulhchc@gmail.com>
2021-04-28 10:41:42 +0530

    commit

--- Ideathon/App.js
+++ Ideathon/App.js
@@ -2,17 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Demo from './Screens/Demo'
-import {DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 const theme = {
   ...DefaultTheme,
   roundness: 2,
   colors: {
-    
-    primary: 'rgb(220,75,99)',
-    accent: 'rgb(223,98,51)',
+    ...DefaultTheme.colors,
+    primary: '#939393',
+    accent: '#f1c40',
   },
-  backgroundColor:'rgb(0,0,0)',
 };
+
 export default function App() {
   return (
     <PaperProvider theme={theme}>
