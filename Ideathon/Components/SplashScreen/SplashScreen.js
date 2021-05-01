87af5ad3c5f6fd3d399d22fb2a240b0bc22033f5
ragulhchc <ragulhchc@gmail.com>
2021-05-01 14:34:16 +0530

    splashscreen

---
+++ Ideathon/Components/SplashScreen/SplashScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SplashScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hi....</Text>
+            </View>
+        )
+    }
+}
+
+export default SplashScreen
