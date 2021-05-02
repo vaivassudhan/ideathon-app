901962fcab62a9f805eff8d78d16ffe66f88e1c0
ragulhchc <ragulhchc@gmail.com>
2021-05-02 23:41:12 +0530

    commit

--- Ideathon/Components/Navigation/RootStackScreen.js
+++ Ideathon/Components/Navigation/RootStackScreen.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
-
+import SplashScreen from '../SplashScreen/SplashScreen';
 
 const RootStack = createStackNavigator();
 
 const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator headerMode='none'>
+        <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
     </RootStack.Navigator>
 );
 
