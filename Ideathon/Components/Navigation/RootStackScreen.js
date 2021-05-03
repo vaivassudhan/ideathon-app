90d9a5b2e53dbef52748e84270dad8198a18ce2f
ragulhchc <ragulhchc@gmail.com>
2021-05-03 21:23:17 +0530

    Navigation

--- Ideathon/Components/Navigation/RootStackScreen.js
+++ Ideathon/Components/Navigation/RootStackScreen.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from '../SplashScreen/SplashScreen';
-// import Register from '../Auth/RegisterForm';
+import Register from '../Auth/RegisterForm';
 import Slide from '../Slide/Slide';
+import HomeScreen from '../../Screens/HomeScreen'
 
 const RootStack = createStackNavigator();
 
 const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator headerMode='none'>
         <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
-        {/* <RootStack.Screen name="Register" component={Register}/> */}
+        <RootStack.Screen name="Register" component={Register}/>
         <RootStack.Screen name="Slide" component={Slide}/>
+        <RootStack.Screen name="HomeScreen" component={HomeScreen}/>
     </RootStack.Navigator>
 );
 
