a676bf3879e5b6560b7c3f768b5064a0e574a512
ragulhchc <ragulhchc@gmail.com>
2021-05-03 14:14:54 +0530

    Commit for Slide In RootStack

--- Ideathon/Components/Navigation/RootStackScreen.js
+++ Ideathon/Components/Navigation/RootStackScreen.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from '../SplashScreen/SplashScreen';
+// import Register from '../Auth/RegisterForm';
+import Slide from '../Slide/Slide';
 
 const RootStack = createStackNavigator();
 
 const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator headerMode='none'>
         <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
+        {/* <RootStack.Screen name="Register" component={Register}/> */}
+        <RootStack.Screen name="Slide" component={Slide}/>
     </RootStack.Navigator>
 );
 
