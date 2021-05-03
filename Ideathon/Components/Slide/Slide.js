beaf0c2923b26215b7246b9a9d4bc6448d80a9c1
ragulhchc <ragulhchc@gmail.com>
2021-05-03 14:15:10 +0530

    New Slide Page

---
+++ Ideathon/Components/Slide/Slide.js
@@ -0,0 +1,33 @@
+import React,{useState , createRef,useEffect ,Component} from 'react';
+import { 
+    View, 
+    ScrollView ,
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    ImageBackground
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { LinearGradient } from 'expo-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+import { useTheme } from 'react-native-paper';
+
+export class Slide extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hi....... , This Page is for Slide</Text>
+            </View>
+        )
+    }
+}
+
+export default Slide
