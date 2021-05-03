019d6ff1c8442b53c7e7358ee8138535caf3466d
ragulhchc <ragulhchc@gmail.com>
2021-05-03 21:23:07 +0530

    Drawer

---
+++ Ideathon/Components/Drawer/DrawerContent.js
@@ -0,0 +1,159 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import React,{useState , createRef, Component , useEffect} from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+    useTheme,
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from 'react-native-paper';
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from '@react-navigation/drawer';
+import { AuthContext } from '../context';
+
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+export function DrawerContent(props)  {
+
+    const paperTheme = useTheme();
+    const { signOut ,toggleTheme} = React.useContext(AuthContext);
+    const [Name,setName]=useState('')
+    const [Gender,setGender]=useState('')
+
+    useEffect(() => {
+        readData()
+    }, [])
+
+    const readData = async() => {
+        try {
+          userName = await AsyncStorage.getItem('Name');
+          userGender = await AsyncStorage.getItem('Gender');
+          userAge = await AsyncStorage.getItem('Age');
+          userPhoneNumber = await AsyncStorage.getItem('PhoneNumber');
+          setName(userName)
+          setGender(userGender)
+          
+        } catch(e) {
+          console.log(e);
+        }
+    }
+
+    return(
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View style={{flexDirection:'row',marginTop: 15}}>
+                            <Avatar.Image 
+                                 source={require('../../assets/cc.png')}
+                                 size={50}
+                            />
+                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                                <Title style={styles.title}>{Name}</Title>
+                                <Caption style={styles.caption}>@{Gender}</Caption>
+                            </View>
+                        </View>
+                    </View>
+
+                    <Drawer.Section style={styles.drawerSection}>
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="home-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Home"
+                            onPress={() => {props.navigation.navigate('Home')}}
+                        />  
+                             
+                        
+
+
+
+                    </Drawer.Section>
+                    {/* <Drawer.Section title="Preferences">
+                        <TouchableRipple onPress={() => {toggleTheme()}}>
+                            <View style={styles.preference}>
+                                <Text>Dark Theme</Text>
+                                <View pointerEvents="none">
+                                    <Switch value={paperTheme.dark}/>
+                                </View>
+                            </View>
+                        </TouchableRipple>
+                    </Drawer.Section> */}
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem 
+                    icon={({color, size}) => (
+                        <Icon 
+                        name="exit-to-app" 
+                        color={color}
+                        size={size}
+                        />
+                    )}
+                    label="Sign Out"
+                    onPress={() => {signOut()}}
+                />
+            </Drawer.Section>
+        </View>
+    );
+    
+}
+
+const styles = StyleSheet.create({
+    drawerContent: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingLeft: 20,
+    },
+    title: {
+      fontSize: 16,
+      marginTop: 3,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+    },
+    row: {
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    section: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    paragraph: {
+      fontWeight: 'bold',
+      marginRight: 3,
+    },
+    drawerSection: {
+      marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#ffff',
+        borderTopWidth: 1
+    },
+    preference: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+  });
