8ece28bbeef64b5e51ae343e3adde6e88398f750
ragulhchc <ragulhchc@gmail.com>
2021-05-04 10:25:29 +0530

    Symptoms Page

---
+++ Ideathon/Screens/Symptoms.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { 
+    SafeAreaView,
+    View, 
+    ScrollView ,
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    Image,
+    ImageBackground
+} from 'react-native';
+
+export class Symptoms extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hi, This is Symptoms Page.....</Text>
+            </View>
+        )
+    }
+}
+
+export default Symptoms
