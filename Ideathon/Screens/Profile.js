936b2929080e7af9ba754164311771ff6746e2ca
ragulhchc <ragulhchc@gmail.com>
2021-05-04 12:10:45 +0530

    Profile

---
+++ Ideathon/Screens/Profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { 
+    View, 
+    ScrollView ,
+    Text, 
+    Image,
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    ImageBackground
+} from 'react-native';
+
+export class Profile extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This Is Profile Page</Text>
+            </View>
+        )
+    }
+}
+
+export default Profile
