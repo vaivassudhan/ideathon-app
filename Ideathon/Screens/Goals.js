de10c898ba2b982a40a4a2decc958cd0b648eff7
ragulhchc <ragulhchc@gmail.com>
2021-05-04 10:25:09 +0530

    Goals Page

---
+++ Ideathon/Screens/Goals.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { 
+    SafeAreaView,
+    View, 
+    ScrollView ,
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    Image,
+    ImageBackground
+} from 'react-native';
+export class Breathing extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hi,THis is Goals Page</Text>
+            </View>
+        )
+    }
+}
+
+export default Breathing
