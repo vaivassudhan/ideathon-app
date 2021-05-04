fca067164f44cc3f67244ff075dc4382bc32d39f
ragulhchc <ragulhchc@gmail.com>
2021-05-05 00:14:43 +0530

    helpline child key error fixed..

--- Ideathon/Data/helplines.js
+++ Ideathon/Data/helplines.js
@@ -1,66 +1,81 @@
 const helpline_nums=[
     {
+        key:"one",
         state:'Andhra Pradesh',
         number:'0866-2410978'
     },
     {
+        key:"two",
         state:'Arunachal Pradesh',
         number:'9436055743'
     },
     {
+        key:"three",
         state:'Assam',
         number:'6913347770'
     },
     {
+        key:"four",
         state:'Bihar',
         number:'104'
     },
-    {
+    {   key:"five",
         state:'Chhattisgarh',
         number:'104'
     },
     {
+        key:"six",
         state:'Goa',
         number:'9436055743'
     },
     {
+        key:"seven",
         state:'Gujarat',
         number:'6913347770'
     },
     {
+        key:"eight",
         state:'Maharashtra',
         number:'020-26127394'
     },
     {
+        key:"nine",
         state:'Tamil Nadu',
         number:'044-29510500'
     },
     {
+        key:"ten",
         state:'Telangana',
         number:'104'
     },
     {
+        key:"eleven",
         state:'Tripura',
         number:'0381-2315879'
     },
 
     {
+        key:"twelve",
         state:'Tamil Nadu',
         number:'044-29510500'
     },
     {
+        key:"thirteen",
         state:'Telangana',
         number:'104'
     },
     {
+        key:"fourtheen",
         state:'Tripura',
         number:'0381-2315879'
     },
     {
+        key:"fifteen",
         state:'Uttar Pradesh',
         number:'18001805145'
     },
     {
+        key:"sixteen",
         state:'West Bengal',
         number:'03323412600'
     },
