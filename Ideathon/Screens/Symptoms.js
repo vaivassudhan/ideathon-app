ebfcaff85d4dcc0e0b42c1f1bde31d9483a3c18c
ragulhchc <ragulhchc@gmail.com>
2021-05-04 22:05:34 +0530

    Symtom

--- Ideathon/Screens/Symptoms.js
+++ Ideathon/Screens/Symptoms.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { 
     SafeAreaView,
     View, 
@@ -13,28 +13,27 @@ import {
     Image,
     ImageBackground
 } from 'react-native';
-import { Button } from 'react-native-paper';
-import _ from "lodash";
-import {SelectMultipleButton} from 'react-native-selectmultiple-button'
-import { Checkbox } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { AuthContext } from '../Components/context';
-
-const ios_blue = "#007AFF";
-const multipleData = ["Fever", "Cold", "Loss of smell", "Loss of taste", "Headache","Pain in chest with deep breaths","Shortness of breath","Sore Throat","Stomach ache","Diarrhea"];
-// const multipleData = {1:"Fever",2: "Cold",3: "Loss of smell",4: "Loss of taste",5: "Headache",6:"Pain in chest with deep breaths",7:"Shortness of breath",8:"Sore Throat",9:"Stomach ache",10:"Diarrhea"};
-export class Symptoms extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      multipleSelectedData: [],
-      multipleSelectedDataLimited: [],
-    };
-  }
-    render() {
-        return (
-            <View styles={styles.container}>
+import { TagSelect } from 'react-native-tag-select';
+import { Button } from 'react-native-paper';
+ 
+export default class App extends React.Component {
+  render() {
+    const data = [
+      { id: 1, label: 'Fever' },
+      { id: 2, label: 'Cold' },
+      { id: 3, label: 'Loss of smell' },
+      { id: 4, label: 'Loss of taste' },
+      { id: 5, label: 'Headache' },
+      { id: 6, label: 'Pain in chest with deep breaths' },
+      { id: 7, label: 'Shortness of breath' },
+      { id: 8, label: 'Sore Throat' },
+      { id: 9, label: 'Stomach ache' },
+      { id: 10, label: 'Diarrhea' },
+    ];
+ 
+    return (
+      <View styles={styles.container}>
                 <View style={{backgroundColor:'rgb(54,118,203)'}}>
                     <StatusBar
                     animated={true}
@@ -47,111 +46,54 @@ export class Symptoms extends Component {
                     <Text style={styles.headtext}>
                     Please select your symptoms
                     </Text>
-                    {multipleData.map(interest => (
                     <View style={styles.symptom_list}>
-                        <SelectMultipleButton
-                        key={interest}
-                        buttonViewStyle={{
-                            borderRadius: 10,
-                            height: 40
+                    <TagSelect
+                        data={data}
+                        ref={(tag) => {
+                              this.tag = tag;
                         }}
-                        textStyle={{
-                            fontSize: 18
-                        }}
-                        highLightStyle={{
-                            borderColor: "gray",
-                            backgroundColor: "transparent",
-                            textColor: "black",
-                            borderTintColor: ios_blue,
-                            backgroundTintColor: ios_blue,
-                            textTintColor: "white"
-                        }}
-                        value={interest}
-                        selected={this.state.multipleSelectedData.includes(interest)}
-                        singleTap={valueTap =>
-                            this._singleTapMultipleSelectedButtons(interest)
-                        }
-                        />
-                        </View>
-                    ))}
-                
+                      />     
+                      </View>
                 </View>
                 <View style={{alignSelf:'flex-end'}}>
-                <Button style={[styles.nextbutton,{backgroundColor:'rgb(34,88,163)'}]} mode="contained" onPress={() => this.props.navigation.navigate('Symco',{selected:this.state.multipleSelectedData})}>
+                <Button style={[styles.nextbutton,{backgroundColor:'rgb(34,88,163)'}]} mode="contained" onPress={() => this.props.navigation.navigate('Symco',{selected:JSON.stringify(this.tag.itemsSelected)})}>
                         Next
                 </Button>
                 </View>
             </View>
-            
-        )
-    }
-
-    _singleTapRadioSelectedButtons(valueTap, gender) {
-        // Alert.alert('', valueTap)
-        this.setState({
-          radioSelectedData: gender
-        });
-      }
-    
-      _singleTapMultipleSelectedButtons(interest) {
-        if (this.state.multipleSelectedData.includes(interest)) {
-          _.remove(this.state.multipleSelectedData, ele => {
-            return ele === interest;
-          });
-        } else {
-          this.state.multipleSelectedData.push(interest);
-        }
-    
-        this.setState({
-          multipleSelectedData: this.state.multipleSelectedData
-        });
-      }
-    
-      _singleTapMultipleSelectedButtons_limited(interest) {
-        if (this.state.multipleSelectedDataLimited.includes(interest)) {
-          _.remove(this.state.multipleSelectedDataLimited, ele => {
-            return ele === interest;
-          });
-        } else {
-          if (this.state.multipleSelectedDataLimited.length < 3)
-            this.state.multipleSelectedDataLimited.push(interest);
-        }
-    
-        this.setState({
-          multipleSelectedDataLimited: this.state.multipleSelectedDataLimited
-        });
-      }
+    );
+  }
 }
-
-export default Symptoms
+ 
 const styles= StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+  container:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  incont:{
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  headtext: {
+      margin: 10,
+      marginTop: 30,
+      fontSize:18,
+      alignSelf:'center'
+      
     },
-    incont:{
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headtext: {
-        margin: 10,
-        marginTop: 30,
-        fontSize:18,
-        alignSelf:'center'
-        
-      },
-      symptom_list:{
-        marginLeft:44,
-        marginRight:44,
+    symptom_list:{
+      marginTop:30,
+      marginLeft:50,
+      marginRight:50,
 
-      },
-      nextbutton:{
-        // position: 'absolute',
-        alignSelf: 'flex-end',
-        top:50,
-        bottom:0,
-        right:20,
-      }
-   
+    },
+    nextbutton:{
+      // position: 'absolute',
+      alignSelf: 'flex-end',
+      top:50,
+      bottom:0,
+      right:20,
+    }
+ 
 })
