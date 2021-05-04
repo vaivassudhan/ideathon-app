c6b8bed9de06d33e54a2bd3fea242f6e90639cf5
ragulhchc <ragulhchc@gmail.com>
2021-05-05 00:13:16 +0530

    Register Form Statusbar color

--- Ideathon/Screens/Report.js
+++ Ideathon/Screens/Report.js
@@ -14,9 +14,11 @@ import {
     ImageBackground
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { Avatar, Button, Card, Title, Paragraph, Divider } from 'react-native-paper';
 export class Breathing extends Component {
     render() {
         return (
+    <View>
         <View style={{backgroundColor:'rgb(54,118,203)'}}>
             <StatusBar
             animated={true}
@@ -25,8 +27,41 @@ export class Breathing extends Component {
             <Icon.Button name="arrow-back-sharp" size={25} backgroundColor="rgb(54,118,203)" onPress={() => this.props.navigation.navigate('Home')}> Back</Icon.Button>   
             </SafeAreaView>
         </View>
+                <Card style={styles.card}>
+               <Card.Content>
+                   <Title style={styles.title}>Ragul</Title>
+                   <Divider/>
+                   <Paragraph style={{fontWeight: 'bold'}}>Name<Paragraph style={{color: '#34495e'}}> :     Ragul</Paragraph></Paragraph>
+               </Card.Content>
+            </Card>   
+    </View>
         )
     }
 }
 
 export default Breathing
+
+const styles = StyleSheet.create({
+card: {
+    alignContent:'center',
+    margin: 16,
+    marginTop:15,
+    backgroundColor:'#fff',
+    borderRadius:12,
+    marginBottom:15,
+    width:'93%',
+    elevation: 6,
+  },
+    paragraph: {
+      margin: 2,
+      fontSize: 25,
+      fontWeight: 'bold',
+      textAlign: 'center',
+     // color: '#34495e',
+
+    },
+    cardText:{
+      fontSize:30,
+      padding:10
+    }
+});
