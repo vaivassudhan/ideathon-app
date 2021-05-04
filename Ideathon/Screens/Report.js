2ab7d69c6232e040a3e8e036d13db29b19dffc27
ragulhchc <ragulhchc@gmail.com>
2021-05-04 10:25:19 +0530

    Report Page

---
+++ Ideathon/Screens/Report.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { 
+    SafeAreaView,
+    View, 
+    ScrollView ,
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    Image,
+    ImageBackground
+} from 'react-native';
+
+export class Breathing extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hi,THis is Report Page</Text>
+            </View>
+        )
+    }
+}
+
+export default Breathing
