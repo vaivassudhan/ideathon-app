0f43dc41dd40aaf0cf4423c9432bab4f9114a372
vaivassudhan <vaivassaravanan1@gmail.com>
2021-05-04 20:20:18 +0530

    helplines

--- Ideathon/Data/helplines.js
+++ Ideathon/Data/helplines.js
@@ -1,3 +1,68 @@
 export default helpline_nums=[
-    {}
+    {
+        state:'Andhra Pradesh',
+        number:'0866-2410978'
+    },
+    {
+        state:'Arunachal Pradesh',
+        number:'9436055743'
+    },
+    {
+        state:'Assam',
+        number:'6913347770'
+    },
+    {
+        state:'Bihar',
+        number:'104'
+    },
+    {
+        state:'Chhattisgarh',
+        number:'104'
+    },
+    {
+        state:'Goa',
+        number:'9436055743'
+    },
+    {
+        state:'Gujarat',
+        number:'6913347770'
+    },
+    {
+        state:'Maharashtra',
+        number:'020-26127394'
+    },
+    {
+        state:'Tamil Nadu',
+        number:'044-29510500'
+    },
+    {
+        state:'Telangana',
+        number:'104'
+    },
+    {
+        state:'Tripura',
+        number:'0381-2315879'
+    },
+
+    {
+        state:'Tamil Nadu',
+        number:'044-29510500'
+    },
+    {
+        state:'Telangana',
+        number:'104'
+    },
+    {
+        state:'Tripura',
+        number:'0381-2315879'
+    },
+    {
+        state:'Uttar Pradesh',
+        number:'18001805145'
+    },
+    {
+        state:'West Bengal',
+        number:'03323412600'
+    },
+
 ]
