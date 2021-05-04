0ad76f61a557c02cdd26726c1fe344b631c576da
ragulhchc <ragulhchc@gmail.com>
2021-05-04 12:10:21 +0530

    Helpline

---
+++ Ideathon/Screens/Helpline.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { 
+    View, 
+    ScrollView ,
+    Text, 
+    Image,
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    ImageBackground
+} from 'react-native';
+
+export class Helpline extends Component {
+    render() {
+        return (
+            <View>
+            <Text>This Is HelpLine Page</Text>
+        </View>
+        )
+    }
+}
+
+export default Helpline
