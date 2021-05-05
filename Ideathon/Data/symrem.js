5757fe76eb99bf89f58286e82494a62d382cdb44
Shreinik Jain <1029384756hhh@gmail.com>
2021-05-05 13:07:28 +0530

    Symptoms and remedies ready

---
+++ Ideathon/Data/symrem.js
@@ -0,0 +1,63 @@
+const symrem =[
+    {
+        symptom:'Fever',
+        allo:'Paracetamol, Acetaminophen and ibuprofen work instantly on fever',
+        homeo:'Arsenicum Album and Belladonna are the best Homeopathic medicine for fever, cold, sore throat and many other diseases',
+        ayur:'Crushed Garlic and Ginger in lemon tea are the widely used remedy for fever'
+    },
+    {
+        symptom:'Cold',
+        allo:'Antihistamines, Throat Lozenges and Menthol give instant relief in case of cold',
+        homeo:'Arsenicum Album and Natrum muriaticum are the best Homeopathic medicine for cold, sore throat and many other diseases',
+        ayur:'Crushed Garlic, Ginger in lemon tea and cinnamon are the widely used remedy for fever'
+    },
+    {
+        symptom:'Loss of Smell',
+        allo:'Consult a doctor',
+        homeo:'Consult a doctor',
+        ayur:'Consult a doctor'
+    },
+    {
+        symptom:'Loss of Taste',
+        allo:'Consult a doctor',
+        homeo:'Consult a doctor',
+        ayur:'Consult a doctor'
+    },
+    {
+        symptom:'Headache',
+        allo:'Paracetamol, Acetaminophen and ibuprofen work instantly on fever',
+        homeo:'Arsenicum Album is the best Homeopathic medicine for headache, cold, sore throat and many other diseases',
+        ayur:'Yoga is the best way to calm your mind'
+    },
+    {
+        symptom:'Pain in chest with deep breaths',
+        allo:'Consult a doctor',
+        homeo:'Consult a doctor',
+        ayur:'Consult a doctor'
+    },
+    {
+        symptom:'Shortness of breath',
+        allo:'Consult a doctor',
+        homeo:'Consult a doctor',
+        ayur:'Consult a doctor'
+    },
+    {
+        symptom:'Sore Throat',
+        allo:'Get a dry throat cough syrup or throat lozenges for instant cure',
+        homeo:'Arsenicum Album and Euphrasia are the best Homeopathic medicine for sore throat, fever, cold and many other diseases',
+        ayur:''
+    },
+    {
+        symptom:'Stomach ache',
+        allo:'Get a antacid in the case of acidity or gas',
+        homeo:'Nux vomica is a homeopathic medicine to cure stomach ache',
+        ayur:'Extract 1 teaspoonful juice of fresh ginger and mix it with 1/2 a teaspoonful of clarified butter.Or boil about a teaspoonful of Ajwain seeds in 1 cup (about 125 ml) of water for 5 minutes. Strain it through the tea strainer and add a pinch of common salt.'
+    },
+    {
+        symptom:'Diarrhoea',
+        allo:'Replacing lost fluids with an oral rehydration solution (ORS) may help to prevent dehydration. Antidiarrhoeal drugs such as loperamide may also help.',
+        homeo:'Arsenicum album, Podophyllum peltatum',
+        ayur:'Curd, Ginger & Honey, Fenugreek seeds, Water of cumin seeds'
+    },
+]
+export default symrem
