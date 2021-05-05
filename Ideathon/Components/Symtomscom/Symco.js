4cf87f5398e3d8725dfaedc779f0881a71c23475
Shreinik Jain <1029384756hhh@gmail.com>
2021-05-05 17:10:30 +0530

    symco changes

--- Ideathon/Components/Symtomscom/Symco.js
+++ Ideathon/Components/Symtomscom/Symco.js
@@ -15,7 +15,7 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { AuthContext } from '../context';
-import { Button } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph, Divider  } from 'react-native-paper';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useEffect } from 'react';
 export default function Symco(props) { 
@@ -34,7 +34,10 @@ export default function Symco(props) {
     global.symp=JSON.parse(props.route.params.selected)
     global.a=[]
     for(var i=0;i<symp.length;i++){
-        a.push(symp[i].label)
+        global.a.push(symp[i].label)
+    }
+    if(global.a.length>3){
+        console.log('Consult a Doctor')
     }
 }, [props.route.params.selected])
 
@@ -72,7 +75,29 @@ const readData = async() => {
                     </SafeAreaView>
                 </View>
                 <View>
-
+                    <Card style={styles.card}>
+                        
+                            {global.a.length>3 && (
+                                <Card.Content>
+                                <Title style={styles.title}>Please consult a Doctor</Title>
+                                <Divider/>
+                                <Paragraph style={{fontWeight: 'bold'}}>Helplines<Paragraph style={{color: '#34495e'}}> : go to helpline page</Paragraph></Paragraph>
+                                </Card.Content>
+                            )}
+                            {global.a.length<=3 && symp.map((u,i)=>{
+                                return (
+                                <Card.Content>
+                                <Title style={styles.title}>Remedy for </Title>
+                                <Divider/>
+                                <Paragraph style={{fontWeight: 'bold'}}>Allo<Paragraph style={{color: '#34495e'}}> : show remedies</Paragraph></Paragraph>
+                                </Card.Content>
+                                )}
+                            )}
+                        <Divider/>
+                        <Card.Content>
+                        <Paragraph style={{fontWeight: 'bold'}}>Status<Paragraph style={{color: '#000080',fontWeight:'bold'}}> : On Progress With SSE</Paragraph></Paragraph>
+                        </Card.Content>
+                    </Card>
                 </View>
                 <View style={{alignSelf:'flex-end'}}>
                 <Button style={[styles.nextbutton,{backgroundColor:'rgb(34,88,163)'}]} mode="contained" onPress={() => Done()}>
@@ -83,34 +108,37 @@ const readData = async() => {
     )
 }
 
-const styles= StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    incont:{
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headtext: {
-        margin: 10,
-        marginTop: 30,
-        fontSize:18,
-        alignSelf:'center'
-        
-      },
-      symptom_list:{
-        marginLeft:44,
-        marginRight:44,
+const styles = StyleSheet.create({
+    card: {
+        alignContent:'center',
+        margin: 16,
+        marginTop:15,
+backgroundColor:'#fff',
+borderRadius:12,
+marginBottom:15,
+width:'93%',
+elevation: 6,
+},
+paragraph: {
+  margin: 2,
+  fontSize: 25,
+  fontWeight: 'bold',
+  textAlign: 'center',
+ // color: '#34495e',
 
-      },
-      nextbutton:{
-        // position: 'absolute',
-        alignSelf: 'flex-end',
-        top:50,
-        bottom:0,
-        right:20,
-      }
-   
-})
+},
+cardText:{
+  fontSize:30,
+  padding:10
+},
+text_header: {
+    color: '#34495e',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign:'center'
+  },
+  title_nodata:{
+    fontWeight: 'bold',
+    textAlign:'center',
+  },
+});
