10071d7036a39356591b208ad749ab74dd4cd828
ragulhchc <ragulhchc@gmail.com>
2021-05-10 18:18:30 +0530

    Styles

---
+++ Ideathon/styles.js
@@ -0,0 +1,91 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  title: {
+    fontSize: 30,
+    lineHeight: 39,
+    marginLeft: 30,
+    marginTop: 60,
+  },
+  firstbutton: {
+    marginTop: 48,
+    alignSelf: "center",
+  },
+  buttons: {
+    marginTop: 30,
+    alignSelf: "center",
+    marginBottom: 5,
+  },
+  taskCategory: {
+    fontSize: 40,
+    lineHeight: 39,
+    alignSelf: "center",
+    marginTop: 30,
+    color: "#8AC6D1",
+  },
+  taskTitle: {
+    fontSize: 46,
+    lineHeight: 48,
+    alignSelf: "center",
+    marginTop: 50,
+    color: "#82BCC7",
+  },
+  rewardTitle: {
+    fontSize: 30,
+    lineHeight: 39,
+    marginLeft: 30,
+    marginTop: 60,
+  },
+  text: {
+    fontSize: 28,
+    textAlign: "center",
+    marginTop: 110,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+  },
+  tasks: {
+    fontSize: 28,
+    textAlign: "center",
+    marginTop: 30,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+  },
+  tasks2: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 30,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+  },
+  text2: {
+    fontSize: 28,
+    textAlign: "center",
+    marginTop: 30,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+  },
+  image: {
+    alignSelf: "center",
+    marginTop: 60,
+    resizeMode: "contain",
+    flex: 1,
+  },
+  container: {
+    padding: 6,
+    backgroundColor: "#ffffff",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 2,
+    paddingHorizontal: 25,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    flex: 2,
+  },
+});
