7cf4f7cadd967e331a00bcb4bad6058bbc110cfa
ragulhchc <ragulhchc@gmail.com>
2021-05-10 12:05:32 +0530

    Help line Updated...

--- Ideathon/Screens/Helpline.js
+++ Ideathon/Screens/Helpline.js
@@ -15,39 +15,50 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import helpline_nums from '../Data/helplines.js'
-export class Helpline extends Component {
-    render() {
+import Feather from 'react-native-vector-icons/Feather';
+import AsyncStorage from '@react-native-community/async-storage';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Avatar, Button, Card, Title, Paragraph, Divider } from 'react-native-paper';
+export function Helpline () {
         return (
-                <View style={styles.container}>
+                <View>
                 <ScrollView>
                 { helpline_nums.map((item, key)=>(
                     <View key={key}>
-                    <View>
-                    <Text key={key} style={styles.statename}>{item.state}</Text>
-                    <View style={{flexDirection:'row'}}>
-                        <Icon style={styles.callicon} onPress={() => {Linking.openURL(`tel:${item.number}`)}} name="call" size={25} />
-                        <Text style={styles.number}  onPress={() => {Linking.openURL(`tel:${item.number}`)}}>{item.number}</Text>
-                    </View>
-                    <View
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
+                    <Card style={styles.card}>
+                <Card.Title
+                    title={<Title  style={styles.title}>{item.state}</Title>}
+                    left={()=><FontAwesome 
+                        name="map-marker"
+                        color="rgb(224,91,40)"
+                        size={30}
+                    />}
+                    right={()=>
+                        <TouchableOpacity onPress = {()=>{Linking.openURL(`tel:${item.number}`)}}>                            
+                        <FontAwesome 
+                        style={{marginRight:30}}
+                        name="phone"
+                        color="rgb(97,212,203)"
+                        size={30}
+                        />
+                        </TouchableOpacity>
+                        }
                     />
-                    </View>
-                    </View>
+            </Card>
+                    
+        </View>
                 ))}
                     </ScrollView>
                 </View>
         )
     }
-}
 export default Helpline
 const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        margin:30
-    },
+    // container:{
+    //     margin:4,
+    //     alignItems:"center",
+    //     justifyContent:'center',
+    // },
     statename:{
         fontSize:20,
         fontWeight:'500',
@@ -68,5 +79,51 @@ const styles=StyleSheet.create({
         marginLeft:10,
         marginBottom:12,
         color:'rgb(54,118,203)'
-    }
+    },
+    card: {
+        marginLeft:"8%",
+            marginRight:"8%",
+            width: '85%',
+            marginTop:8,
+            marginBottom:2,
+            flexDirection:'row',
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 4,
+            },
+            shadowOpacity: 0.30,
+            shadowRadius: 11.95,
+            borderRadius:20,
+    
+            elevation: 8,
+      },  
+      title:{
+        fontWeight: 'bold',
+        marginRight:20,
+        color:'rgb(52,125,247)'
+      },
+        paragraph: {
+          margin: 2,
+          fontSize: 25,
+          fontWeight: 'bold',
+          textAlign: 'center',
+        //color: '#34495e',
+    
+        },
+        cardText:{
+          fontSize:30,
+          padding:10
+        },
+        text_header: {
+            color: '#34495e',
+            fontWeight: 'bold',
+            fontSize: 20,
+            textAlign:'center',
+            marginTop:12,
+          },
+          title_nodata:{
+            fontWeight: 'bold',
+            textAlign:'center',
+          },
 })
