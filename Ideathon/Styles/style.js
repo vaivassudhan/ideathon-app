01f73b740e9d7401e839322ab30c63d5c61d35cb
ragulhchc <ragulhchc@gmail.com>
2021-05-13 20:26:24 +0530

    App js added location in local storage

--- Ideathon/Styles/style.js
+++ Ideathon/Styles/style.js
@@ -2,4 +2,9 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     
+    bgcard:"#1a1a2e",
+    text:"white",
+    secondarytext:"#e94560",
+    button:"#0f3460"
+
 });
