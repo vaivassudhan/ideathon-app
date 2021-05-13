b5b7821fe0fe44f959efb3f567dfcda9ec708d9f
ragulhchc <ragulhchc@gmail.com>
2021-05-13 20:50:37 +0530

    Home Screen

--- Ideathon/Screens/HomeScreen.js
+++ Ideathon/Screens/HomeScreen.js
@@ -168,7 +168,7 @@ const styles= StyleSheet.create({
     container:{
         flex:1,
         alignItems:'center',
-        backgroundColor:style[0].bgcard
+        backgroundColor:'#fcfcfc'
     },
     welcometext:{
         fontSize:20,
